d7654428ac906e52f0c682a32b2ea8a71ab74e21
ym2737 <ym2737@an098068-ct103v.nvstu.edu>
2015-01-24 11:48:53 -0500

    completed quiz 1

---
+++ Testing1/src/Testing1/Quiz1.java
@@ -0,0 +1,20 @@
+package Testing1;
+import java.util.Scanner;
+
+public class Quiz1 {
+	
+	public static void main(String[] args) {
+		String userName;
+		String password;
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter your username");
+		userName = input.next();
+		System.out.println("Enter you password");
+		password = input.next();
+		
+		
+		System.out.println("Hello "+ userName +", Welcome to CSC200 class! and Your password is "+ password);
+		input.close();
+	}
+
+}
