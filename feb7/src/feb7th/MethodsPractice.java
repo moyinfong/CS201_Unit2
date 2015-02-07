5fd7532dcb23d072004b9806693676f9bf028e71
YinFong <YinFong@PandaSurface.nvstu.edu>
2015-02-07 10:53:18 -0500

    initial version

---
+++ feb7/src/feb7th/MethodsPractice.java
@@ -0,0 +1,55 @@
+package feb7th;
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+
+public class MethodsPractice {
+	static String correctionUsername = "moyinfong";
+	static String correctionPassword = "9876";
+	static String username;
+	static String password;
+	static Scanner input = new Scanner(System.in);
+	static boolean status = false;
+
+	public static void main(String[] args) {
+		
+		authentication(password, username);
+	
+		}
+		
+	
+	public static boolean authentication(String password, String username){
+			status = false;
+			System.out.println("Enter your username");
+			username = input.next();
+			System.out.println("Enter your password");
+			password = input.next();
+			if(username.equalsIgnoreCase(correctionUsername) && password.equals(correctionPassword)){
+				System.out.println("Welcome "+ username+ "!" );
+			status = true;
+			}
+			else{
+			status = false;
+				System.out.println("Invalid Password or Username");
+			}
+		
+		return status;
+		}
+	
+	public static boolean authenication2(String password,String username){
+		status = false;
+				
+		if(username.equalsIgnoreCase(correctionUsername)&& password.equals(correctionPassword)){
+			JOptionPane.showMessageDialog(null, "Welcome "+ username);
+			status = true;
+			
+		}
+		else{
+			JOptionPane.showMessageDialog(null,"Invalid Password or Username");
+		}
+		
+		return status;
+		
+		
+	}
+	}
---
+++ feb7/src/feb7th/MethodsPractice2.java
@@ -0,0 +1,15 @@
+package feb7th;
+import javax.swing.JOptionPane;
+
+public class MethodsPractice2 {
+
+	public static void main(String[] args) {
+		
+		MethodsPractice testing = new MethodsPractice();
+		String username = JOptionPane.showInputDialog("Enter Username");
+		String password = JOptionPane.showInputDialog("Enter Password");
+		testing.authenication2(password, username);
+
+	}
+
+}
